d21edfb76a7a5a86ec7feb896353eb31ca0807b1
Nikita <akkforgames2020@gmail.com>
2024-12-16 04:41:18 +0500

    first

---
+++ Iterative/Iterative.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <clocale>
+#include <math.h>
+#define E 2.71828
+
+using namespace std;
+
+static double f(double x) {
+	return pow(E, x) - pow(E, -x) - 2;
+}
+
+static double f1(double x) {
+	return log(pow(E, -x) + 2);
+}
+
+static double f2(double x) {
+	return -1/(2*pow(E, x) + 1);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, x0, x1;
+	double e = 0.001;
+	cin >> a >> b;
+	if (abs(f2(a)) < 1) {
+		x0 = a;
+	}
+	else {
+		x0 = b;
+	}
+	cout << "В ходе вычислений программа находит вот такие приближения:" << endl;
+	x1 = f1(x0);
+	while (abs(x0 - x1) >= e) {
+		cout << "x = " << x1 << endl;
+		x0 = x1;
+		x1 = f1(x0);
+	}
+	cout << "А примерным итоговым корнем будет являться: " << x1 << endl;
+	return 0;
+}
---
+++ Newton/Newton.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <clocale>
+#include <math.h>
+#define E 2.71828
+
+using namespace std;
+
+static double f(double x) {
+	return pow(E, x) - pow(E, -x) - 2;
+}
+
+static double f1(double x) {
+	return pow(E, x) + pow(E, -x);
+}
+
+static double f2(double x) {
+	return pow(E, x) - pow(E, -x);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, x0 = 0.0, x1;
+	double e = 0.001;
+	cin >> a >> b;
+	if (f(a) * f2(a) > 0) {
+		x0 = a;
+	}
+	else {
+		x0 = b;
+	}
+	x1 = x0 - f(x0) / f1(x0);
+	cout << "В ходе вычислений программа находит вот такие приближения:" << endl;
+	while (abs(x1 - x0) >= e) {
+		cout << "x = " << x1 << endl;
+		x0 = x1;
+		x1 = x0 - f(x0) / f1(x0);
+	}
+
+	cout << "А примерным итоговым корнем будет являться: " << x1 << endl;
+	return 0;
+}
